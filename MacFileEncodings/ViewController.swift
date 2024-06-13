d2595f83bd1f17d30cb77e9c5f588a7c183bcefd
Semyon Ishkhanyan <semyon.ishkhanyan@gmail.com>
2024-06-14 01:57:17 +0300

    project added

---
+++ MacFileEncodings/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MacFileEncodings
+//
+//  Created by Семён Ишханян on 14.06.2024.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
